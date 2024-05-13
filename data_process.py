e6f3731d2c891e2c10a4b1d7838c0a80b5771078
Giorgio David Aversa <gaversa@linux7.cs.uchicago.edu>
2024-05-13 12:12:08 -0500

    Updated data process structure

--- data_process.py
+++ data_process.py
@@ -16,14 +16,15 @@ class Data_Process():
     def __init__(self):
 
         ###need to change folder every time, directory where photos and csv is
-        self.data_dir = '/home/gaversa/catkin_ws/src/final_project_soccer_bot'
+        self.data_dir = '/home/gaversa/catkin_ws/src/final_project_soccer_bot/data/data_coll_5_9'
 
         #Not sure which image type we can use, whether PIL works or not
         self.load_images()
 
         self.expert_dset = None
-        self.images = []
-        self.velocities = []
+        self.runs = []
+        #self.images = []
+        #self.velocities = []
 
     def load_images(self):
         """
@@ -32,39 +33,44 @@ class Data_Process():
         
         https://pytorch.org/hub/pytorch_vision_resnet/
         """
-
-
-        ###rename to fit our naming convention
-        photo_dir = self.data_dir + '/photos'
-        csv_dir = self.data_dir + '/data_coll_5_8'
-
-        df = pd.read_csv(csv_dir)
-        cur_idx = 0
+        cur_run = 0
+        images = []
+        velocities = []
 
         #see os walk documentation if more info needed
-        for (dirpath, dirnames, filenames) in walk(photo_dir):
+        for (top_dirpath, top_dirnames, top_filenames) in walk(photo_dir):
+            for (dirpath, dirnames, filenames) in walk(top_dirnames):
+                if filenames.startswith('image'):
+                    ###Uses PIL Image, verify if PIL format is usable for us
+                    input_image = Image.open(filenames)
+
+                    preprocess = transforms.Compose([
+                        transforms.Resize(256),     #change values
+                        transforms.CenterCrop(224), #change values
+                        transforms.ToTensor(),
+                        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                    ])
+
+                    input_tensor = preprocess(input_image)
+                    
+                    ###Unsure if this is needed (or how much image processesing is required)
+                    #input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+
+                
+                    images.append(input_tensor)
+                    velocities.append(actions)
+
+                elif filenames.endswith('.csv'):
+                    df = pd.read_csv(filenames)
+                    for index, row in df.iterrows():
+                        velocities.append(row['Linear Velocity'], row['Angular Velocity'])
+                        
             
-            ###Uses PIL Image, verify if PIL format is usable for us
-            input_image = Image.open(filenames)
-
-            preprocess = transforms.Compose([
-                transforms.Resize(256),     #change values
-                transforms.CenterCrop(224), #change values
-                transforms.ToTensor(),
-                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-            ])
-
-            input_tensor = preprocess(input_image)
-            #input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
-
-            ### Assumes df rows are loaded with indexing lining up to the order that photos are iterated over
-            actions = (df['Linear Velocity'].iloc[cur_idx], df['Angular Velocity'].iloc[cur_idx])
-            #self.expert_dset = ExpertDataSet(input_batch, actions)
-            self.images.append(input_tensor)
-            self.velocities.append(actions)
-
-            cur_idx += 1
-
+            ExpertDataSet(cur_run, images, velocities)
+            cur_run += 1
+            images = []
+            velocities = []
+            
 
 
 
@@ -72,14 +78,18 @@ class Data_Process():
 
 class ExpertDataSet(Dataset):
    
-   def __init__(self, expert_observations, expert_actions):
+   def __init__(self, index, image_list, csv_list):
         """
-        Expert Data Set where self.observations is a [list? batch? of multiple?
-        /or single?] image(s) and self.actions is a tuple of the linear and 
-        angular velocity for the specified image at a given location
+        Data set containing the data for a single run.
+
+        Index stores what number run
+        Observations stores all images as a list in chronological order
+        Actions stores all velocities read from the csv as a tuple pair 
+        (Linear, Angular) in order of each time stamp
         """
-        self.observations = expert_observations
-        self.actions = expert_actions
+        self.index = index
+        self.observations = image_list
+        self.actions = csv_list
 
    def __getitem__(self, index):
         return (self.observations[index], self.actions[index])
